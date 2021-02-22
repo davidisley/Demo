56ae68ba5528aeaa3e5e31e29bd007944aeea428
David Isley <isley.david@gmail.com>
2021-02-21 21:15:27 -0500

    calling a js file

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
-<script>
-    alert("Hello world");
-</script>
+// When the user presses submit button, change the greeting to include their name //
+
+// get access to the button and set up a click event handler 
+var button = document.getElementById("submit");
+button.onclick = changeGreeting;
+
+function changeGreeting(){
+    var greeting = document.getElementsByTagName("h1")[0];
+    var input = document.getElementById("name").value;
+    greeting.innerHTML = "Hello, " + input;
+}
